c1985679d74b99df0f0e9aa0a1fe78185f9a1aeb
benshutt <shuttbenjamin@gmail.com>
2020-02-22 16:05:44 +0000

    Script to use base directory as default with option to provide an explicit path to the font directory

--- FontManagerApp/fontManagerApp.sh
+++ FontManagerApp/fontManagerApp.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
+# Usage description if invalid arguments provided
+usage() { echo "Usage: $ [-p <font directory path>]" 1>&2; exit 1; }
+
 # Font file extension
 EXTENSION=ttf
 
@@ -23,19 +26,33 @@ CLONE_DIRECTORY=${BASE_DIRECTORY}/FMFiles
 # Handle cases when there are no matching files
 shopt -s nullglob
 
-# Get directory of input
-read -p "Enter font directory path: " FONT_DIRECTORY
+# Directory where the font files are locacted, by default use base directory.
+# User may override with optional command line argument
+FONT_DIRECTORY=${BASE_DIRECTORY}
+
+# Handle any arguments provided
+while getopts "p:" o; do
+    case "${o}" in
+        p)
+            FONT_DIRECTORY=${OPTARG}
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1)) # Shift off the options and optional --.
 
 # Check directory exists
 if [ ! -d "${FONT_DIRECTORY}" ]; then
-    echo "'${FONT_DIRECTORY}' directory does not exist"
+    echo "'${FONT_DIRECTORY}' directory does not exist" 1>&2;
     exit 1
 fi
 
 # Get the files of the valid extension type
 FONT_FILES=( ${FONT_DIRECTORY}/*.${EXTENSION} )
 if [ ${#FONT_FILES[@]} -eq 0 ]; then
-    echo "No files matching extension: '${EXTENSION}' in directory: '${FONT_DIRECTORY}'"
+    echo "No files matching extension: '${EXTENSION}' in directory: '${FONT_DIRECTORY}'" 1>&2;
     exit 1
 fi
 
@@ -47,7 +64,7 @@ done
 
 # Prepare clean up
 function cleanup {
-    echo "Cleaning"
+    echo "Cleaning process"
     rm -rf ${CLONE_DIRECTORY}
 }
 
@@ -61,7 +78,5 @@ git clone ${CLONE_URL} ${CLONE_DIRECTORY}
 cd ${CLONE_DIRECTORY}
 git checkout develop
 cd ${APP_NAME}
-pwd
 
 swift run ${APP_NAME} ${FONT_DIRECTORY}
-
